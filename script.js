daa8f2804fba56ba5a960eed27ce865004e8b8de
Nathan Rosenboom <nathan.rosenboom@gmail.com>
2020-03-27 14:00:46 +0800

    Added function that places markers on the map based on event coordinates

--- script.js
+++ script.js
@@ -1,72 +1,91 @@
 
 // Initialize and add the map
 var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('content'), {
-          center: {lat: -32.000, lng: 115.800},
-          zoom: 10
-        });
-        infoWindow = new google.maps.InfoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('content'), {
+        center: { lat: -32.000, lng: 115.800 },
+        zoom: 10
+    });
+    infoWindow = new google.maps.InfoWindow;
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
             var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
             };
-
+            console.log(position.coords.latitude);
+            console.log(position.coords.longitude);
             infoWindow.setPosition(pos);
             infoWindow.setContent('You are here');
             infoWindow.open(map);
             map.setCenter(pos);
-          }, function() {
+        }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    setMarkers(map);
+}
+// Array to hold event coordinates
+var eventMarkers = [
+    ['event name1', -31.523, 115.456],
+    ['event name2', -31.727, 115.856],
+    ['event name3', -31.783, 115.806]
+];
+console.log(eventMarkers);
+// Function to place event location markers on map
+function setMarkers(map){
+    for (var i = 0; i < eventMarkers.length; i++){
+        var eventMarker = eventMarkers[i];
+        var marker = new google.maps.Marker({
+            position: {lat: eventMarker[1], lng: eventMarker[2]},
+            map: map,
+            title: eventMarker[0]
+        });
+    }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
 
-    
-//Trying the ticketmaster api
+// Trying the ticketmaster api
 var apikey = 'RFICjUjeerutWCLOjlYKQaGawIvVOZ6R';
 
-//Script for options popover
+// Generate options popover
 let currentPopover = null;
 
-    const buttons = document.querySelectorAll('ion-button');
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', handleButtonClick);
-    }
+const buttons = document.querySelectorAll('ion-button');
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', handleButtonClick);
+}
 
-    async function handleButtonClick(ev) {
-      popover = await popoverController.create({
+async function handleButtonClick(ev) {
+    popover = await popoverController.create({
         component: 'options-popover',
         event: ev,
         translucent: true
-      });
-      currentPopover = popover;
-      return popover.present();
-    }
+    });
+    currentPopover = popover;
+    return popover.present();
+}
 
-    function dismissPopover() {
-      if (currentPopover) {
+function dismissPopover() {
+    if (currentPopover) {
         currentPopover.dismiss().then(() => { currentPopover = null; });
-      }
     }
+}
 
-    customElements.define('options-popover', class ModalContent extends HTMLElement {
-      connectedCallback() {
+customElements.define('options-popover', class ModalContent extends HTMLElement {
+    connectedCallback() {
         this.innerHTML = `
         <ion-list>
         <!--List of checkbox options-->
@@ -101,5 +120,5 @@ let currentPopover = null;
         </ion-item>
     </ion-list>
         `;
-      }
-    });
+    }
+});
